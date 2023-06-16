847a01adc8cc5a54fd81a0e3fb8f5ca8be55d11d
Kingsley Victor <javaprodigy56@gmail.com>
2023-06-16 16:14:28 +0100

    finish

--- app/ui/Header/index.tsx
+++ app/ui/Header/index.tsx
@@ -2,12 +2,19 @@ import { CTAPurple } from "@/components/Button";
 import { FiMenu } from "react-icons/fi";
 import Image from "next/image";
 import Link from "next/link";
+import { useRef } from "react";
 
 export default function Header() {
+  const checkRef = useRef<HTMLInputElement>(null);
   return (
     <div className="flex justify-between items-center w-full px-5 py-4 bg-[linear-gradient(90.67deg,_rgba(12,_14,_30,_0.5)_6.48%,_rgba(20,_23,_46,_0.5)_58.12%,_rgba(217,_217,_217,_0)_99.86%)] lg:bg-none">
       <div className="flex justify-center items-center gap-3">
-        <button className="flex lg:hidden justify-center items-center gap-2 bg-[#131735] border-0 outline-0 h-full px-2 py-2 text-[#fff] rounded-[7px] text-[1.2em]">
+        <button
+          onClick={() => {
+            if (checkRef.current) checkRef.current.checked = true;
+          }}
+          className="flex lg:hidden justify-center items-center gap-2 bg-[#131735] border-0 outline-0 h-full px-2 py-2 text-[#fff] rounded-[7px] text-[1.2em]"
+        >
           <FiMenu />
         </button>
         <div className="flex justify-center gap-1 items-center">
@@ -21,6 +28,27 @@ export default function Header() {
         <Link href="/">staking</Link>
         <Link href="/">roadmap</Link>
         <Link href="/">contact us</Link>
+      </div>
+      <input ref={checkRef} id="sidebar" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-side lg:hidden">
+        <label htmlFor="sidebar" className="drawer-overlay"></label>
+        <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content capitalize font-[500] text-[0.95em]">
+          <li>
+            <Link href="/">home</Link>
+          </li>
+          <li>
+            <Link href="/">launchpad</Link>
+          </li>
+          <li>
+            <Link href="/">staking</Link>
+          </li>
+          <li>
+            <Link href="/">roadmap</Link>
+          </li>
+          <li>
+            <Link href="/">contact us</Link>
+          </li>
+        </ul>
       </div>
       <div className="flex justify-center items-start">
         <CTAPurple
